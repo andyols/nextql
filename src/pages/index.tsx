7d3459b5b061c3906b107d68e344e98854a16cf0
andyols <andyols.dev@gmail.com>
2022-06-06 11:24:03 -0500

    ⚡perf: implement next/image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,27 +5,29 @@ import {
 } from "graphql/generated"
 import { getRequestClient } from "lib/client"
 import type { NextPage } from "next"
+import Image from "next/image"
 import Link from "next/link"
 
 const variables: PokemonQueryVariables = { pokemon: PokemonEnum["Pikachu"] }
+const size = 80
 
 const Home: NextPage = () => {
   const { data, isLoading } = usePokemonQuery(getRequestClient(), variables)
 
   return (
-    <div className="flex justify-center w-screen h-screen">
-      <div className="w-full max-w-3xl pt-16">
-        <Link href={"/ssr"}>
-          <a className="hover:underline hover:cursor-pointer font-semibold">
-            ssr page
-          </a>
-        </Link>
-        <div className="flex flex-col pt-4">
-          <img src={data?.getPokemon.sprite} className="w-20" />
-          <p className="font-mono">
-            {isLoading ? "loading..." : JSON.stringify(data?.getPokemon)}
-          </p>
-        </div>
+    <div className="container pt-16 mx-auto">
+      <Link href={"/ssr"}>
+        <a className="hover:underline hover:cursor-pointer font-semibold">
+          ssr page
+        </a>
+      </Link>
+      <div className="flex flex-col items-start w-8/12 pt-4 space-y-4">
+        {data?.getPokemon.sprite && (
+          <Image src={data.getPokemon.sprite} width={size} height={size} />
+        )}
+        <p className="font-mono">
+          {isLoading ? "loading..." : JSON.stringify(data?.getPokemon)}
+        </p>
       </div>
     </div>
   )
